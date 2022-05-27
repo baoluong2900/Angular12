6adda31f72cbb8ad90606cc9de9e936f0e20f938
baoluong2900 <baoluong0209@gmail.com>
2022-05-27 13:08:16 +0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
+// Cấu hình ngModel
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { OnsalePipe } from './onsale.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    OnsalePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // Import cấu hình ngModel
+    BrowserModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { getLocaleDateTimeFormat } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,7 +10,25 @@ export class HomeComponent implements OnInit {
   public name ='Hello bao';
   constructor() { }
 	public age =25;
-  ngOnInit(): void {
+  public RestName() {
+    console.log("Hello")
+    this.name='';
   }
+  public traicay =['Táo','Nho','Cam','Quit','Xoài','Ổi'];
+  public dateNow = new Date().toLocaleString();
+  public lstraicay =[
+    {Name:'Táo', Price:10000, Quantity:10,CreateDate:this.dateNow, Active:true}
+    , {Name:'Nho', Price:20000, Quantity:12, CreateDate:this.dateNow, Active:false}
+    , {Name:'Cam', Price:30000, Quantity:15,CreateDate:this.dateNow, Active:true}
+    , {Name:'Quit', Price:40000, Quantity:-3,CreateDate:this.dateNow, Active:false}
+    , {Name:'Xoài', Price:50000, Quantity:11,CreateDate:this.dateNow, Active:true}
+  ];
+  ngOnInit(): void {
+    console.log('traicay = ', this.traicay)
+    console.log('lstraicay = ', this.lstraicay)
+  }
+
+
+
 
 }
